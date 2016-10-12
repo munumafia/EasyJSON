371a70778560330e5730acf1a76c6de17c578d17
Sean Grimes <sean.grimes@praeses.com>
2016-10-12 07:48:02 -0500

    Set up gulp watch to automatically compile TypeScript files

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,20 @@
 "use strict"
 
-let gulp = require('gulp');
-let ts = require('gulp-typescript');
+let gulp = require("gulp");
+let sourcemaps = require("gulp-sourcemaps");
+let ts = require("gulp-typescript");
 let tsProject = ts.createProject("tsconfig.json");
 
 gulp.task("default", () => {
-    return tsProject.src()
-        .pipe(tsProject())
-        .js.pipe(gulp.dest("dist"));
+    let tsResult = tsProject.src()
+        .pipe(sourcemaps.init())
+        .pipe(tsProject());
+
+    return tsResult.js
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest("dist"))
+});
+
+gulp.task("watch", ["default"], () => {
+    gulp.watch("*.ts", ["default"]);
 });
