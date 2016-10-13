d63dd2ac9b5b65622f2d7544d5c3537cfea86114
Sean Grimes <sean.grimes@praeses.com>
2016-10-13 16:40:54 -0500

    Misc changes

--- src/Parser.ts
+++ src/Parser.ts
@@ -1,29 +1,41 @@
 import {Token} from "./Lexer";
 import {TokenType} from "./Lexer";
-import {TokenToSymbolMapper, Document, ISymbol} from "./Symbols"
+import * as symbols from "./Symbols"
 import {Node, LinkedList} from "./LinkedList"
 
 export class Parser {
-    private document : Document;
+    private document : symbols.Document;
     private tokens : Token[] = [];
 
     public constructor(tokens : Token[], programText : string) {
         this.tokens = tokens;
-        this.document = new Document(programText);
+        this.document = new symbols.Document(programText);
     }
 
     public parse() : Document {
         let document = this.document;
-        let mapper = new TokenToSymbolMapper();
+        let mapper = new symbols.TokenToSymbolMapper();
 
         // First round of parsing, convert all tokens to symbols and create
         // a linked list
-        let symbolList = new LinkedList<ISymbol>();
-        let symbols = this.tokens.map(token => {
+        let symbolList = new LinkedList<symbols.ISymbol>();
+        this.tokens.forEach(token => {
             let symbol = mapper.map(token)
-            let node = new Node<ISymbol>(symbol);
+            let node = new Node<symbols.ISymbol>(symbol);
             symbolList.push(node);
         });
+
+        // Parse from the bottom up
+        let node = symbolList.tail;
+        while (node.previous != null) {
+            if (node.value.symbolType == symbols.SymbolType.EqualSign) {
+                let equalSign = node.value as symbols.EqualSign;
+                equalSign.leftHandSide = node.previous.value;
+                equalSign.rightHandSide = node.next.value;
+
+                [node.next, node.previous].forEach(symbolList.removeNode);
+            }
+        }
 
         return null;
     }
